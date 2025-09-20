0b9a8c7bd29b149ae2f3774e2401c598db031b42
devsalomao <christophersalomao5@gmail.com>
2025-09-20 11:23:31 -0300

    feat: estrurua inicial da página home  criada

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import { ThemeSwitch } from "./themeSwitch";
 import { Container } from "./container";
+import { FiUser, FiLogOut } from "react-icons/fi";
+import Link from "next/link";
 
 export function Header () {
   return (
-    <header className="w-full flex items-center justify-between bg-blue-950 h-25">
+    <header className="w-full flex items-center justify-between bg-blue-950 h-25 shadow-sm  dark:shadow-white/25">
       <Container>
         <div className="w-full h-full flex items-center justify-between">
-          <div className="relative h-16 w-1/10">
+          <Link href="/" className="relative h-16 w-26 sm:w-40 hover:scale-105 transition-all duration-300">
             <Image
               src={"/logo.png"}
               alt="Logo Sigma Suport"
@@ -16,8 +17,16 @@ export function Header () {
               fill
               className="object-contain"
             />
+          </Link>
+
+          <div className="flex items-center gap-4">
+            <Link href="/painel">
+              <FiUser size={26} color="white" />
+            </Link>
+            <button>
+              <FiLogOut size={26}  color="white"/>
+            </button>
           </div>
-          <ThemeSwitch />
         </div>
       </Container>
     </header>
--- src/components/themeSwitch.tsx
+++ src/components/themeSwitch.tsx
@@ -25,7 +25,7 @@ export function ThemeSwitch() {
   return (
     <button
       onClick={toggleTheme}
-      className="relative flex h-8 w-16 items-center rounded-full bg-gray-200 p-1 transition-colors duration-300 dark:bg-gray-800 shadow-md shadow-purple-400 *:transition-all *:ease-in-out *:duration-300 dark:shadow-amber-400"
+      className=" flex h-8 w-16 items-center rounded-full bg-gray-200 p-1 transition-colors duration-300 dark:bg-gray-800 shadow-md shadow-purple-400 *:transition-all *:ease-in-out *:duration-300 dark:shadow-amber-400 absolute bottom-4 md:top-8.5 right-4"
     >
       <span
         className={`absolute flex h-6 w-6 items-center justify-center rounded-full bg-white shadow-md transition-transform duration-300 ${
