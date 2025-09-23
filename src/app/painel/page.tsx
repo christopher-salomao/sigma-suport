dacea17ae74cf08eb7152c8cda42842ca6f19948
devsalomao <christophersalomao5@gmail.com>
2025-09-23 09:05:54 -0300

    feat: rota /painel privada apenas para usuario autenticados

--- src/app/painel/page.tsx
+++ src/app/painel/page.tsx
@@ -1,6 +1,16 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
 import { Container } from "@/components/container";
 
-export default function painel() {
+export default async function painel() {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
+
   return (
     <main className="w-full">
       <Container>
--- src/components/themeSwitch.tsx
+++ src/components/themeSwitch.tsx
@@ -28,7 +28,7 @@ export function ThemeSwitch() {
       className=" flex h-8 w-16 items-center rounded-full bg-gray-200 p-1 transition-colors duration-300 dark:bg-gray-800 shadow-md shadow-purple-400 *:transition-all *:ease-in-out *:duration-300 dark:shadow-amber-400 absolute bottom-4 md:top-8.5 right-4"
     >
       <span
-        className={`absolute flex h-6 w-6 items-center justify-center rounded-full bg-white shadow-md transition-transform duration-300 ${
+        className={`md:absolute fixed flex h-6 w-6 items-center justify-center rounded-full bg-white shadow-md transition-transform duration-300 ${
           theme === "light" ? "translate-x-0" : "translate-x-8"
         }`}
       >
